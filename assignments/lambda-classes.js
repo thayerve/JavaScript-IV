1b432aa9916398aaefc3e518590a902ab4212546
Virginia Thayer <virginia@stripe.com>
2019-06-13 14:38:47 -0700

    submitting finished MVP projects, including objects in each Lambda class, and logs and method calls to test.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -21,6 +21,7 @@ class Instructor extends Person {
         super(instAttributes);
         this.specialty = instAttributes.specialty;
         this.favLanguage = instAttributes.favLanguage;
+        this.catchPhrase = instAttributes.catchPhrase;
     }
 
     demo(subject) {
@@ -52,13 +53,14 @@ class Student extends Person {
         console.log(`${this.name} has submitted a PR for ${subject}`);
     }
 
-    sprintChallenge() {
+    sprintChallenge(subject) {
         console.log(`${this.name} has begun sprint challenge on ${subject}`);
     }
 }
 
 class ProjectManager extends Instructor {
     constructor(pmAttributes) {
+        super(pmAttributes);
         this.gradClassName = pmAttributes.gradClassName;
         this.favInstructor = pmAttributes.favInstructor;
     }
@@ -79,7 +81,7 @@ class ProjectManager extends Instructor {
 const danLevy = new Instructor({
     name: 'Dan Levy',
     location: 'Denver?',
-    age: '32',
+    age: 32,
     favLanguage: 'JavaScript',
     specialty: 'FSW',
     catchPhrase: `Don't listen to Rosie`
@@ -88,7 +90,7 @@ const danLevy = new Instructor({
 const joshKnell = new Instructor({
     name: 'Josh Knell',
     location: 'Georgia?',
-    age: '31',
+    age: 31,
     favLanguage: 'Ruby',
     specialty: 'banjo',
     catchPhrase: `You got this!`
@@ -97,16 +99,51 @@ const joshKnell = new Instructor({
 const avni = new Student({
     name: 'Avni Patel Thompson',
     location: 'Seattle',
-    age: '28',
-    favSubjects: ''
+    age: 28,
+    className: 'WEB21',
+    previousBackground:`Founder & CEO of Poppy`,
+    favSubjects: ['Node.js', 'Summer Hackers', 'Array methods'],
 });
 
 const virginia = new Student({
     name: 'Virginia Thayer',
     location: 'Seattle',
-    age: 36;
-    previousBackground: ,
+    age: 36,
+    previousBackground: 'User Operations at Stripe',
     className: 'WEB21',
     favSubjects: ['LESS', 'JavaScript', 'Pseudo-classical inheritance']
 });
+
+const joshuaSmoot = new ProjectManager({
+    name: 'Joshua Smoot',
+    age: 26,
+    location: 'Charleston',
+    favInstructor: 'Josh Knell',
+    gradClassName: 'WEB17'
+});
+
+const mary = new ProjectManager({
+    name: 'Mary',
+    age: 24,
+    gradClassName: 'WEB18',
+    favInstructor: 'Josh Knell',
+    location: 'New York',
+    specialty: 'Express and Node.js',
+    favLanguage: 'Python',
+    catchPhrase: "That looks AWESOME",
+});
+
+console.log(joshuaSmoot.gradClassName);
+console.log(mary.favInstructor);
+console.log(mary.favLanguage);
+console.log(danLevy.catchPhrase);
+joshKnell.demo('JavaScript');
+danLevy.grade(virginia, 'Class Constructors');
+joshuaSmoot.standUp('#web21_Smoot');
+avni.speak();
+virginia.PRAssignment('JavaScript IV');
+joshKnell.speak();
+virginia.sprintChallenge('LESS');
+mary.grade(avni, 'HTML');
+avni.listsSubjects();
 
