9f662355983f1a6c100659d708c79bfbc966a83f
Virginia Thayer <virginia@stripe.com>
2019-06-13 11:04:16 -0700

    set up Lambda Classes with inheritance, attributes defined, and empty methods

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,2 +1,66 @@
 // CODE here for your Lambda Classes
 
+class Person {
+    constructor(personAttributes){
+        this.name = personAttributes.name;
+        this.age = personAttributes.age;
+        this.location = personAttributes.location;
+    }
+
+    speak(){
+        console.log(`Hello my name is ${this.name}`, I am from ${this.location});
+    }
+}
+
+class Instructor extends Person {
+    constructor(instAttributes){
+        super(instAttributes);
+        this.specialty = instAttributes.specialty;
+        this.favLanguage = instAttributes.favLanguage;
+    }
+    
+    demo(){
+
+    }
+
+    grade() {
+
+    }
+}
+
+class Student extends Person {
+    constructor(studAttributes){
+        super(studAttributes);
+        this.previousBackground = studAttributes.previousBackground;
+        this.className = studAttributes.className;
+        this.favSubjects = studAttributes.favSubjects;
+    }
+
+    listsSubjects(){
+
+    }
+
+    PRAssignment(){
+
+    }
+
+    sprintChallenge(){
+
+    }
+}
+
+class ProjectManager extends Instructor {
+    constructor(pmAttributes) {
+        this.gradClassName = pmAttributes.gradClassName;
+        this.favInstructor = pmAttributes.favInstructor;
+    }
+
+    standUp(){
+
+    }
+
+    debugsCode(){
+        
+    }
+
+}
